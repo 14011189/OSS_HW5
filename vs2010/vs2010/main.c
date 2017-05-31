ab8efa20248d33cd8947ee94134aa73cf4e48267
HyunJung <dnflchfhrdl@naver.com>
2017-05-31 18:05:50 +0900

    vsfdf

--- vs2010/vs2010/main.c
+++ vs2010/vs2010/main.c
@@ -16,30 +16,6 @@
 
 #define dim 18
 
-void clear_screen ( void )
-{
-  DWORD n;                         /* Number of characters written */
-  DWORD size;                      /* number of visible characters */
-  COORD coord = {0};               /* Top left screen position */
-  CONSOLE_SCREEN_BUFFER_INFO csbi;
-
-  /* Get a handle to the console */
-  HANDLE h = GetStdHandle ( STD_OUTPUT_HANDLE );
-
-  GetConsoleScreenBufferInfo ( h, &csbi );
-
-  /* Find the number of characters to overwrite */
-  size = csbi.dwSize.X * csbi.dwSize.Y;
-
-  /* Overwrite the screen buffer with whitespace */
-  FillConsoleOutputCharacter ( h, TEXT ( ' ' ), size, coord, &n );
-  GetConsoleScreenBufferInfo ( h, &csbi );
-  FillConsoleOutputAttribute ( h, csbi.wAttributes, size, coord, &n );
-
-  /* Reset the cursor to the top left position */
-  SetConsoleCursorPosition ( h, coord );
-}
-
 void boss(int vet[dim][dim],int dif)
 {
     int i,l,num,flag,k,spara;
